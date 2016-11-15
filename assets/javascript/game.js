7652fd9a57a8022fa447e0b070de32e8cbbf958f
Brenton Lake <Brenton Lake>
2016-11-14 19:27:51 -0600

    update with beginning js

---
+++ assets/javascript/game.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+
+    var counter = 0;
+    var wins = 0;
+    var losses = 0;
+    var score = 0;
+    $('#score').text(score);
+    $('#win').text(wins);
+    $('#loss').text(losses);
+
+    crystals = ['assets/images/crystal1.jpg','assets/images/crystal2.jpg','assets/images/crystal3.jpg','assets/images/crystal4.jpg'];
+
+    
+    
+
+   
+
+
+
+
+
+
+    });
