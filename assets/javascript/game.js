e84b6c32d6b1d98c172cfe2f2acb7ecc91b0f0e5
Brenton Lake <Brenton Lake>
2016-11-16 16:38:32 -0600

    beautified

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,71 +1,70 @@
 $(document).ready(function() {
 
-
+    //Variables
     var counter = 0;
     var wins = 0;
     var losses = 0;
     var score = 0;
-    var random =randomIntFromInterval(19,120);
+    var random = randomIntFromInterval(19, 120);
+
+    //Setting scores on html
     $('#randnum').text(random);
-	$('#score').text(counter);
+    $('#score').text(counter);
     $('#score').text(score);
     $('#win').text(wins);
     $('#loss').text(losses);
 
-    var crystal1 = 0;
-    var crystal2 = 0;
-    var crystal3 = 0;
-    var crystal4 = 0;
+    var crystal1;
+    var crystal2;
+    var crystal3;
+    var crystal4;
 
 
+    //crytal clicks
     $('#img1').click(function() {
-    	crystal1 = Math.floor((Math.random()*12)+1);
+        crystal1 = Math.floor((Math.random() * 12) + 1);
 
-    	console.log(crystal1);
-    	$("#score").text(crystal1);
-
-
+        console.log(crystal1);
+        $("#score").text(crystal1);
 
 
-});
+    });
 
     $('#img2').click(function() {
-    	crystal2 = Math.floor((Math.random()*12)+1);
-    	console.log(crystal2);
-    	$("#score").text(crystal2);
+        crystal2 = Math.floor((Math.random() * 12) + 1);
+        console.log(crystal2);
+        $("#score").text(crystal2);
 
 
-});
+    });
     $('#img3').click(function() {
-    	crystal3 = Math.floor((Math.random()*12)+1);
-    	console.log(crystal3);
-    	$("#score").text(crystal3);
-    	
-    	
+        crystal3 = Math.floor((Math.random() * 12) + 1);
+        console.log(crystal3);
+        $("#score").text(crystal3);
 
 
-
-});
+    });
     $('#img4').click(function() {
-    	crystal4 = Math.floor((Math.random()*12)+1);
-    	console.log(crystal4);
-    	$("#score").text(crystal4);
+        crystal4 = Math.floor((Math.random() * 12) + 1);
+        console.log(crystal4);
+        $("#score").text(crystal4);
+
+
+    });
+
+    function randomIntFromInterval(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    $('#win')
+    if (random === score);
+    $('win').text(wins++);
+
+    $('#loss')
+    if (random > score);
+    $('#loss').text(losses++);
+
+
 
 
 });
-
-    function randomIntFromInterval(min,max){
-	return Math.floor(Math.random()*(max-min+1)+min);
-}
-  
-
-		   
-	
-    
-
-   
-
-
-
-});
-
